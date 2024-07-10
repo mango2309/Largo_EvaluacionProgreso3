9a3f3245843494e0a9bc26337ce73cb219acbd4a
mango2309 <sebastian.largo@udla.edu.ec>
2024-07-10 12:19:35 -0500

    Se realizaron las vistas de la interfaz

---
+++ Largo_EvaluacionP3/Views_SL/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace Largo_EvaluacionP3
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+    }
+
+}
